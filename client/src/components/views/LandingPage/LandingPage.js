43584e5efe363e45a96d090e838458dcdad28a09
mook <mook9288@gmail.com>
2022-03-12 01:49:00 +0900

    feat: 랜딩페이지 - 상품카드, 상품이미지슬라이드

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,30 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { FaCode } from 'react-icons/fa';
+import { Card, Row, Col } from 'antd';
+import Meta from 'antd/lib/card/Meta';
+import ImageSlider from '../../utils/ImageSlider';
 
 function LandingPage() {
+  const [products, setProducts] = useState([]);
   useEffect(() => {
     axios.post('/api/product/products').then((response) => {
       if (response.data.success) {
-        console.log('상품 노출');
-        console.log(response.data);
+        console.log('상품 노출', response.data);
+        setProducts(response.data.productInfo);
       } else {
-        alert(' 상품들을 가져오는데 실패 했습니다.');
+        alert('상품들을 가져오는데 실패 했습니다.');
       }
     });
   }, []);
 
+  const renderCards = products.map((product, index) => {
+    return (
+      <Col lg={6} md={8} xs={24} key={index}>
+        <Card
+          cover={
+            <a href={`/product/${product._id}`}>
+              <ImageSlider images={product.images} />
+            </a>
+          }
+        >
+          <Meta title={product.title} description={`$${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
+
   return (
-    <>
-      <div className='app'>
-        <FaCode style={{ fontSize: '4rem' }} />
-        <br />
-        <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+    <div style={{ width: '75%', margin: '3rem auto' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h2>Let's Travel Anywhere </h2>
       </div>
-      <div style={{ float: 'right' }}>
-        Thanks For Using This Boiler Plate by John Ahn
+      {/* Filter */}
+      {/* Search */}
+      {/* Cards */}
+      <Row gutter={[16, 16]}>{renderCards}</Row>
+
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <button>더보기</button>
       </div>
-    </>
+    </div>
   );
 }
 
